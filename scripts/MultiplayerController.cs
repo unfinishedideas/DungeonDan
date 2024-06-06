132c467b50594b4525b43c89ebcbff826f0ae4b8
unfinishedideas <unfinishedideas@gmail.com>
2024-06-06 16:50:56 -0700

    refactored MultiplayerController

--- scripts/MultiplayerController.cs
+++ scripts/MultiplayerController.cs
@@ -9,13 +9,22 @@ public partial class MultiplayerController : Control
 	private int _port = 8910;
 	[Export]
 	private string _address = "127.0.0.1"; 
+
 	[Export]
-	private int _maxClients = 16;
+	private int _maxClients = 4;
 	
 	private ENetMultiplayerPeer _peer;
 	private ENetConnection.CompressionMode _compressionMode = ENetConnection.CompressionMode.Fastlz;
 
-	private bool hosting = false;
+    private const string _localHost = "127.0.0.1";
+	private bool _hosting = false;
+
+    private Label _reportingText;
+    private Label _statusText;
+    private Button _startGameButton;
+    private Button _joinGameButton;
+    private LineEdit _ipEntryEdit;
+    private LineEdit _nameEntryEdit;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -27,12 +36,27 @@ public partial class MultiplayerController : Control
 		Multiplayer.ConnectionFailed += ConnectionFailed;
 		Multiplayer.ServerDisconnected += ServerDisconnected;
 
+        _statusText = GetNode<Label>("Panel/StatusText");
+        _reportingText = GetNode<Label>("Panel/ReportingText");
+        _startGameButton = GetNode<Button>("Panel/StartGame");
+        _joinGameButton = GetNode<Button>("Panel/Join");
+		_ipEntryEdit = GetNode<LineEdit>("Panel/IpEntry");
+		_nameEntryEdit = GetNode<LineEdit>("Panel/NameEntry");
+
+        ClearTextFields();
+
 		// if a dedicated server
 		if (OS.GetCmdlineArgs().Contains("--server"))
 		{
 			hostGame();
 		}
 	}
+
+    private void ClearTextFields()
+    {
+        _statusText.Text = "";
+        _reportingText.Text = "";
+    }
 
 	// Multiplayer Event Functions ---------------------------------------------------------------------------------------
 	/// <summary>
@@ -50,7 +74,7 @@ public partial class MultiplayerController : Control
     {
 		GD.Print("connected to server");
 		// RPC ID makes the RPC to a specific ip. In this case, the server
-		RpcId(1, "sendPlayerInformation", GetNode<LineEdit>("Panel/NameEntry").Text, Multiplayer.GetUniqueId());
+		RpcId(1, "sendPlayerInformation", _nameEntryEdit.Text, Multiplayer.GetUniqueId());
     }
 
 	/// <summary>
@@ -151,6 +175,7 @@ public partial class MultiplayerController : Control
 		if (error != Error.Ok)
 		{
 			GD.Print("failed to host game! : " + error.ToString());
+            _reportingText.Text = error.ToString();
 			return;
 		}
 		_peer.Host.Compress(_compressionMode);
@@ -158,26 +183,26 @@ public partial class MultiplayerController : Control
 		GD.Print("waiting for players!");
 	}
 
-	// Button down signals -----------------------------------------------------------------------------------------------
+	// Signals ----------------------------------------------------------------
 	/// <summary>
 	/// set up a server and then join it, registers host info
 	/// </summary>
 	public void _on_host_button_down()
 	{
-		if (hosting == false)
+		if (_hosting == false)
 		{
 			GameManager.IsMultiplayerGame = true;
-			hosting = true;
+			_hosting = true;
 			hostGame();
-			sendPlayerInformation(GetNode<LineEdit>("Panel/NameEntry").Text, 1);
-			GetNode<Label>("Panel/StatusText").Text = "You are hosting!";
-			GetNode<Button>("Panel/Join").Disabled = true;
-			GetNode<Button>("Panel/StartGame").Disabled = false;
+			sendPlayerInformation(_nameEntryEdit.Text, 1);
+			_statusText.Text = "You are hosting!";
+			_joinGameButton.Disabled = true;
+			_startGameButton.Disabled = false;
 		}
 		else
 		{
             GameManager.IsMultiplayerGame = false;
-            hosting = false;
+            _hosting = false;
 		}
 	}
 
@@ -187,15 +212,15 @@ public partial class MultiplayerController : Control
 	public void _on_join_button_down()
 	{
 		_peer = new ENetMultiplayerPeer();
-		string entered__address = GetNode<LineEdit>("Panel/IpEntry").Text;
-		if (entered__address != null)
-		{
-			_address = entered__address;
-		}
+		string entered_address = _ipEntryEdit.Text;
+        _address = entered_address == "" ? _localHost : entered_address;
+        GD.Print($"Joining with: {_address}");
+        
 		var error = _peer.CreateClient(_address, _port);
 		if (error != Error.Ok)
 		{
 			GD.Print("failed to join game! : " + error.ToString());
+            _reportingText.Text = error.ToString();
 			return;
 		}
 		_peer.Host.Compress(_compressionMode);
