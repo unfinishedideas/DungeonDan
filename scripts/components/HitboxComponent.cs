891f686a667ae6d2cb6ab92eb00aeafa2bf7c6f3
unfinishedideas <unfinishedideas@gmail.com>
2024-06-08 21:23:38 -0700

    Added hitbox component, added custom floatyguy model

---
+++ scripts/components/HitboxComponent.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class HitboxComponent : Area3D
+{
+    [Export]
+    public float Damage = 10f;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
--- scripts/components/HurtboxComponent.cs
+++ scripts/components/HurtboxComponent.cs
@@ -10,21 +10,10 @@ public partial class HurtboxComponent : Area3D
     public delegate void HitByProjectileEventHandler();
 
     [Signal]
-    public delegate void HitByHurtboxEventHandler();
+    public delegate void HitByHitboxEventHandler();
 
     public void Damage(Attack attack)
     {
         _healthComponent.Damage(attack);
-    }
-
-    public void _on_hurtbox_component_body_entered(Node3D body)
-    {
-        GD.Print($"Hurtbox body entered: {body}");
-        EmitSignal(SignalName.HitByHurtbox);
-    }
-
-    public void _on_hurtbox_component_area_entered(Area3D area)
-    {
-        GD.Print($"Hurtbox area entered: {area}");
     }
 }
