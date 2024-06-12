5d0cbe5fbb254daacf0003ddceb6cffc2c03267e
unfinishedideas <unfinishedideas@gmail.com>
2024-06-12 15:32:53 -0700

    Added GodotJolt physics addon (until Godot implements it into the engine directly)

--- scripts/components/HitboxComponent.cs
+++ scripts/components/HitboxComponent.cs
@@ -11,5 +11,5 @@ public partial class HitboxComponent : Area3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
+	} 
 }
