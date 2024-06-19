30f15b1a5c78ea9626dffd7c6febb24a4b4c18ca
unfinishedideas <unfinishedideas@gmail.com>
2024-06-18 19:50:19 -0700

    fixed problem where hitboxes were hurting the owner of the hitbox

--- scripts/components/HurtboxComponent.cs
+++ scripts/components/HurtboxComponent.cs
@@ -13,12 +13,19 @@ public partial class HurtboxComponent : Area3D
         CollisionMask = 2;
     }
 
+    // This is sometimes called from other places
+    public void Damage(Attack attack)
+    {
+        _healthComponent.Damage(attack);
+    }
+
+    // If this doesn't fire, ensure that the signal is connected to root node
     public void _on_area_entered(Area3D area)
     {
-        if (area is HitboxComponent)
+        if (area is HitboxComponent && !Owner.IsAncestorOf(area))
         {
-           HitboxComponent box = (HitboxComponent)area;
-           _healthComponent.Damage(box.HitboxAttack);
+            HitboxComponent box = (HitboxComponent)area;
+            Damage(box.HitboxAttack);
         }
     }
 }
