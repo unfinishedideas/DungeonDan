f06ef8fe887b278b593d0302341593d16293ea6b
unfinishedideas <unfinishedideas@gmail.com>
2024-06-18 17:14:03 -0700

    updating physics layers / collisions to have hitbox/hurtboxes separated. Started hitbox/hurtbox code

--- scripts/components/HitboxComponent.cs
+++ scripts/components/HitboxComponent.cs
@@ -1,15 +1,21 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class HitboxComponent : Area3D
 {
+    [Export]
+    public float Damage;
+    [Export]
+    public float Knockback;
+
+    public Attack HitboxAttack;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        HitboxAttack = new Attack(Damage, Knockback);
+        CollisionLayer = 2;
+        CollisionMask = 0;
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	} 
 }
--- scripts/components/HurtboxComponent.cs
+++ scripts/components/HurtboxComponent.cs
@@ -6,6 +6,12 @@ public partial class HurtboxComponent : Area3D
     [Export]
     private HealthComponent _healthComponent;
 
+    public override void _Ready()
+    {
+        CollisionLayer = 0;
+        CollisionMask = 2;
+    }
+
     public void Damage(Attack attack)
     {
         _healthComponent.Damage(attack);
